196f959400cae0f13cfe28d55dcf28677e896d9a
Valentin Zemlyakov <me@0010.ru>
2023-04-24 16:04:17 +0300

    read input data and print out

--- src/arrays/main.c
+++ src/arrays/main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, const char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Error: too few or too many arguments!\n");
+        fprintf(stderr, "Usage: %s [filename]\n", argv[0]);
+        fprintf(stderr, "Your input: ");
+        for (size_t i = 0; i < argc; i++)
+            fprintf(stderr, "%s ", argv[i]);
+        fprintf(stderr, "\n");
+        return EXIT_FAILURE;
+    }
 
-int main (){
-    printf("Hello, world! There are arrays.\n");
-    return 0;
+    FILE *fp = fopen(argv[1], "r");
+    if (!fp) {
+        fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    char *line = NULL;
+    size_t line_len = 0;
+    ssize_t read;
+
+    while ((read = getline(&line, &line_len, fp)) > 0) {
+        int num = strtol(line, NULL, 10);
+        printf("%d\n", num);
+    }
+
+    if (ferror(fp)) {
+        perror("Error while reading file");
+    }
+
+    free(line);
+    fclose(fp);
+    return EXIT_SUCCESS;
 }
