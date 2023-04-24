8f8243ba12f2d9820d304707c40b7c411d765341
Valentin Zemlyakov <me@0010.ru>
2023-04-24 20:56:18 +0300

    big data

---
+++ test_data.py
@@ -0,0 +1,18 @@
+import random
+
+
+def list_of_ints(n: int, a: int, b: int) -> None:
+    with open("list.txt", "w") as fp:
+        for _ in range(1_000_000):
+            i = random.randint(a=a, b=b)
+            fp.write(f'{i}\n')
+
+
+def main():
+    N = 1_000_000
+
+    list_of_ints(n=N, a=-1000, b=1000)
+
+
+if __name__ == "__main__":
+    main()
