7bc77d2ea92570410451602998d0ac73633bb5b9
Valentin Zemlyakov <me@0010.ru>
2023-04-24 21:13:45 +0300

    some fixes

--- test_data.py
+++ test_data.py
@@ -3,12 +3,12 @@ import random
 
 def list_of_ints(n: int, a: int, b: int) -> None:
     with open("list.txt", "w") as fp:
-        for _ in range(1_000_000):
+        for _ in range(n):
             i = random.randint(a=a, b=b)
             fp.write(f'{i}\n')
 
 
-def main():
+def main() -> None:
     N = 1_000_000
 
     list_of_ints(n=N, a=-1000, b=1000)
