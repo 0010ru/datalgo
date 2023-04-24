1100deeffcc3c78133acbd1d37e9857bc277ac36
Valentin Zemlyakov <me@0010.ru>
2023-04-24 19:42:03 +0300

    some stylish

--- src/arrays/main.c
+++ src/arrays/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <unistd.h>
 
 struct Array {
     int16_t* data;
@@ -11,12 +10,8 @@ struct Array {
 
 void push_back(struct Array* arr, int16_t value) {
     if (arr->size == arr->capacity) {
-        if (arr->capacity >= SIZE_MAX/sizeof(int16_t)) {
-            perror("Error while reallocating memory");
-            exit(EXIT_FAILURE);
-        }
         arr->capacity *= 2;
-        int16_t* tmp = (int16_t*)realloc(arr->data, arr->capacity * sizeof(int16_t));
+        int16_t* tmp = realloc(arr->data, arr->capacity * sizeof(int16_t));
         if (tmp == NULL) {
             perror("Error while reallocating memory");
             exit(EXIT_FAILURE);
@@ -26,45 +21,32 @@ void push_back(struct Array* arr, int16_t value) {
     arr->data[arr->size++] = value;
 }
 
-int main(int argc, const char *argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Error: too few or too many arguments!\n");
+        fprintf(stderr, "Error: invalid number of arguments!\n");
         fprintf(stderr, "Usage: %s [filename]\n", argv[0]);
-        fprintf(stderr, "Your input: ");
-        for (size_t i = 0; i < (size_t)argc; i++)
-            fprintf(stderr, "%s ", argv[i]);
-        fprintf(stderr, "\n");
-
         return EXIT_FAILURE;
     }
 
     FILE *fp = fopen(argv[1], "r");
     if (fp == NULL) {
-        fprintf(stderr, "Cannot open file '%s': ", argv[1]);
-        perror("");
-        fclose(fp);
+        perror("Error while opening file");
+        return EXIT_FAILURE;
+    }
 
+    struct Array arr = {NULL, 0, 52};
+    arr.data = malloc(arr.capacity * sizeof(int16_t));
+    if (arr.data == NULL) {
+        perror("Error while allocating memory");
+        fclose(fp);
         return EXIT_FAILURE;
     }
 
     char *line = NULL;
     size_t line_len = 0;
+
+    // Fill the array with data from the file
     ssize_t read;
-
-    struct Array arr;
-    arr.size = 0;
-    arr.capacity = 52;
-    arr.data = (int16_t*)malloc(arr.capacity * sizeof(int16_t));
-    if (arr.data == NULL) {
-        perror("Error while allocating memory");
-        free(line);
-        free(arr.data);
-        fclose(fp);
-
-        return EXIT_FAILURE;
-    }
-
-    // The array is filled with data from the file
     while ((read = getline(&line, &line_len, fp)) != -1) {
         int16_t num = 0;
         if (sscanf(line, "%hd", &num) == 1) {
@@ -74,32 +56,20 @@ int main(int argc, const char *argv[]) {
             free(line);
             free(arr.data);
             fclose(fp);
-
             return EXIT_FAILURE;
         }
     }
+    free(line);
+    fclose(fp);
 
     // Print all values
-    printf("input data = [");
+    printf("Input data = [");
     for (size_t i = 0; i < arr.size; i++) {
         if (i < arr.size - 1) printf("%hd, ", arr.data[i]);
         else printf("%hd", arr.data[i]);
     }
     printf("]\n");
-
-    if (ferror(fp)) {
-        fprintf(stderr, "Error while reading file '%s': ", argv[1]);
-        perror("");
-        free(line);
-        free(arr.data);
-        fclose(fp);
-
-        return EXIT_FAILURE;
-    }
-
-    free(line);
     free(arr.data);
-    fclose(fp);
 
     return EXIT_SUCCESS;
 }
